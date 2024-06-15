1e5bf66ac169944500f892430a38e6f8f46688f6
oAndin <00andherson@gmail.com>
2024-06-14 22:31:47 -0300

    Last updates

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class SnakeGame extends JPanel implements ActionListener, KeyListener {
@@ -26,16 +27,18 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     int boardWidth;
     int boardHeight;
     int tileSize = 25;
-    Tile snakeHead;
     Random random;
     Tile food;
-
+    // Snake
+    Tile snakeHead;
+    ArrayList<Tile> snakeBody;
 
     // Game logic
 
     Timer gameLoop;
     int velocityX;
-    int velocitiY;
+    int velocityY;
+    boolean gameOver = false;
 
 
     SnakeGame(int boardWidth, int boardHeight){
@@ -47,6 +50,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         setFocusable(true);
 
         snakeHead = new Tile(5,5);
+        snakeBody = new ArrayList<Tile>();
 
         food = new Tile(10,10);
 
@@ -57,7 +61,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         gameLoop = new Timer(100, this);
         gameLoop.start();
 
-        velocitiY = 0;
+        velocityY = 0;
         velocityX = 0;
     }
 
@@ -67,21 +71,41 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     }
 
     public void draw(Graphics g) {
-        //
-        for (int i = 0; i < boardWidth/tileSize; i++) {
-            // (x1,y1,x2,y2)
-            g.drawLine(i * tileSize,0,i*tileSize,boardHeight);
-            g.drawLine(0, i*tileSize,boardWidth,i*tileSize);
-        }
+        // grid
+//        for (int i = 0; i < boardWidth/tileSize; i++) {
+//            // (x1,y1,x2,y2)
+//            g.drawLine(i * tileSize,0,i*tileSize,boardHeight);
+//            g.drawLine(0, i*tileSize,boardWidth,i*tileSize);
+//        }
 
 
         //Food
         g.setColor(Color.red);
-        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+//        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+        g.fill3DRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize, true);
 
-        //Snake
+
+        //Snake Head
         g.setColor(Color.green);
-        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+//        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+        g.fill3DRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize, true);
+
+
+        // Snake Body
+        for( int i = 0; i< snakeBody.size(); i++){
+            Tile snakePart = snakeBody.get(i);
+//            g.fillRect(snakePart.x * tileSize, snakePart.y * tileSize , tileSize, tileSize);
+            g.fill3DRect(snakePart.x * tileSize, snakePart.y * tileSize , tileSize, tileSize, true);
+        }
+        // Score
+        g.setFont(new Font("Arial", Font.PLAIN, 16));
+        if (gameOver) {
+            g.setColor(Color.red);
+            g.drawString("Game Over " + String.valueOf(snakeBody.size()), tileSize- 16, tileSize);
+        }
+        else {
+            g.drawString("Score: " + String.valueOf(snakeBody.size()), tileSize-16, tileSize);
+        }
     }
 
     public void placeFood() {
@@ -89,35 +113,76 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         food.y = random.nextInt(boardHeight/ tileSize); // 600 / 25 -> 24
     }
 
-    public void move() {
-        snakeHead.x += velocityX;
-        snakeHead.y += velocitiY;
+    public boolean collision(Tile tile1, Tile tile2) {
+        return tile1.x == tile2.x && tile1.y == tile2.y;
     }
+
+    public void move() {
+        // Eat food
+        if(collision(snakeHead, food)) {
+            snakeBody.add(new Tile(food.x, food.y));
+            placeFood();
+        }
+
+        // Snake Body
+        for ( int i = snakeBody.size()-1; i >=0; i--){
+            Tile snakePart = snakeBody.get(i);
+            if ( i == 0) {
+                snakePart.x = snakeHead.x;
+                snakePart.y = snakeHead.y;
+            }
+            else {
+                Tile prevSnakePart = snakeBody.get(i-1);
+                snakePart.x = prevSnakePart.x;
+                snakePart.y = prevSnakePart.y;
+            }
+        }
+
+        // Snake Head
+        snakeHead.x += velocityX;
+        snakeHead.y += velocityY;
+
+        // game over conditions
+        for(int i = 0; i < snakeBody.size(); i++) {
+            Tile snakePart = snakeBody.get(i);
+            // collide with the snake head
+            if(collision(snakeHead, snakePart)) {
+                gameOver = true;
+            }
+        }
+        if (snakeHead.x * tileSize < 0 || snakeHead.x*tileSize > boardWidth|| snakeHead.y*tileSize < 0 || snakeHead.y*tileSize > boardHeight) {
+            gameOver = true;
+        }
+    }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
         move();
         repaint();
+        if (gameOver) {
+            gameLoop.stop();
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() ==  KeyEvent.VK_UP){
+        if(e.getKeyCode() ==  KeyEvent.VK_UP && velocityY != 1 ){
             velocityX = 0;
-            velocitiY = -1;
+            velocityY = -1;
         }
-        else if(e.getKeyCode() ==  KeyEvent.VK_DOWN){
+        else if(e.getKeyCode() ==  KeyEvent.VK_DOWN && velocityY != -1){
             velocityX = 0;
-            velocitiY = 1;
+            velocityY = 1;
         }
-        else if(e.getKeyCode() ==  KeyEvent.VK_RIGHT){
-            velocityX = 1;
-            velocitiY = 0;
-        }
-        else if(e.getKeyCode() ==  KeyEvent.VK_LEFT){
+        else if(e.getKeyCode() ==  KeyEvent.VK_LEFT && velocityX != 1){
             velocityX = -1;
-            velocitiY = 0;
+            velocityY = 0;
+        }
+        else if(e.getKeyCode() ==  KeyEvent.VK_RIGHT && velocityX != -1){
+            velocityX = 1;
+            velocityY = 0;
         }
     }
-
 }
